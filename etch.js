c1b9e3817082d53d18c7bb6a97a3ccfd70c250cd
chuchomp <carlosjmatterson@gmail.com>
2023-09-02 22:10:07 -0300

    add clear grid button add resize grid button add styles

--- etch.js
+++ etch.js
@@ -1,18 +1,67 @@
-let container = document.getElementById('container')
+let grid = document.getElementById('grid');
+let resizeValue = document.getElementById('size-input');
+let submit = document.getElementById('submit');
+let clearButton = document.getElementById('clear-grid')
 
-function coloring(grid) {
-    grid.setAttribute('class', 'pressed');
+console.log(submit)
+
+let mouseDown = false;
+
+document.body.onmousedown = () => {
+    (mouseDown = true);
+}
+document.body.onmouseup = () => {
+    (mouseDown = false);
+}
+function coloring(e) {
+    if (e.type === 'mouseover' && !mouseDown) {
+        return
+    }
+    console.log(e.target)
+    e.target.setAttribute('class', 'pressed')
 }
 
 function createGrid() {
-    for (let i = 0; i < 273; i++) {
-    let gridCell = document.createElement('div');
-    gridCell.setAttribute('class', 'grid-cell');
-    gridCell.addEventListener('mousedown', () => {
-        gridCell.addEventListener('mouseover', coloring(gridCell));
-    });
-    container.appendChild(gridCell);
+        grid.style.gridTemplateColumns = `repeat(${resizeValue.value}, 1fr)`
+        grid.style.gridTemplateRows = `repeat(${resizeValue.value}, 1fr)`
+        for (let i = 0; i < (resizeValue.value * resizeValue.value); i++) {
+        let gridCell = document.createElement('div');
+        gridCell.setAttribute('class', 'grid-cell');
+        gridCell.setAttribute('class', 'unpressed')
+        gridCell.addEventListener('mouseover', coloring);
+        grid.appendChild(gridCell);
+        };
+    }
+
+function clearGrid() {
+    let gridCell = document.querySelectorAll('.pressed');
+    Object.values(gridCell).map((item) => 
+    {item.removeAttribute('pressed');
+    item.setAttribute('class', 'unpressed')});
 }
+
+function removeGrid() {
+    grid.innerHTML = '';
 }
+
+clearButton.addEventListener('click', () => {
+    clearGrid();
+    console.log('button working')
+})
+
+submit.addEventListener('click', () => {
+    removeGrid();
+    createGrid();
+    console.log(resizeValue.value);
+})
+//working createGrid
+// function createGrid() {
+//     for (let i = 0; i < 273; i++) {
+//     let gridCell = document.createElement('div');
+//     gridCell.setAttribute('class', 'grid-cell');
+//     gridCell.addEventListener('mouseover', coloring);
+//     grid.appendChild(gridCell);
+//     };
+// }
 
 createGrid();
