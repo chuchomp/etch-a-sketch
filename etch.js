42912959bb4b998a0b887c3a922233f3163ecb4e
chuchomp <carlosjmatterson@gmail.com>
2023-09-03 20:11:37 -0300

    add color picker feature modified elements layout add favicon

--- etch.js
+++ etch.js
@@ -1,9 +1,15 @@
+
+let currentColor = '#333333';
 let grid = document.getElementById('grid');
 let resizeValue = document.getElementById('size-input');
 let submit = document.getElementById('submit');
-let clearButton = document.getElementById('clear-grid')
+let clearButton = document.getElementById('clear-grid');
+let eraserButton = document.getElementById('eraser');
+let markerButton = document.getElementById('marker');
+let rainbowButton = document.getElementById('rainbow');
+let colorPicker = document.getElementById('colorPicker')
 
-console.log(submit)
+resizeValue.value = 16; //default starting size of grid
 
 let mouseDown = false;
 
@@ -13,12 +19,36 @@ document.body.onmousedown = () => {
 document.body.onmouseup = () => {
     (mouseDown = false);
 }
+
+let currentMode = 'marker'
+
+rainbowButton.addEventListener('click', () => {
+    currentMode = 'rainbow'
+} )
+
+eraserButton.addEventListener('click', () => {
+    currentMode = 'eraser';
+})
+
+markerButton.addEventListener('click', () => {
+    currentMode = 'marker';
+})
+
+colorPicker.oninput = (e) => {currentColor = e.target.value}
+
 function coloring(e) {
     if (e.type === 'mouseover' && !mouseDown) {
         return
     }
-    console.log(e.target)
-    e.target.setAttribute('class', 'pressed')
+
+    if (currentMode === 'rainbow') {
+        e.target.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}`
+
+    } else if (currentMode === 'marker') {
+        e.target.style.backgroundColor = currentColor;
+    } else if (currentMode === 'eraser') {
+        e.target.style.backgroundColor = 'white';
+    }
 }
 
 function createGrid() {
@@ -27,17 +57,18 @@ function createGrid() {
         for (let i = 0; i < (resizeValue.value * resizeValue.value); i++) {
         let gridCell = document.createElement('div');
         gridCell.setAttribute('class', 'grid-cell');
-        gridCell.setAttribute('class', 'unpressed')
         gridCell.addEventListener('mouseover', coloring);
+        gridCell.addEventListener('mousedown', coloring);
         grid.appendChild(gridCell);
         };
     }
 
 function clearGrid() {
-    let gridCell = document.querySelectorAll('.pressed');
+    let gridCell = document.querySelectorAll('div.grid-cell');
     Object.values(gridCell).map((item) => 
-    {item.removeAttribute('pressed');
-    item.setAttribute('class', 'unpressed')});
+        {item.style.backgroundColor = 'white'});
+    // {item.removeAttribute('pressed');
+    // item.setAttribute('class', 'unpressed')});
 }
 
 function removeGrid() {
